eb3856b8f272b0d935e768263cf71e51cf3d108b
xalvarex <alvarobalada73@gmail.com>
2023-01-23 21:08:53 +0100

    init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -52,7 +52,7 @@ require('packer').startup(function(use)
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
-  use 'olimorris/onedarkpro.nvim'
+  use 'navarasu/onedark.nvim'
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
@@ -175,7 +175,6 @@ require('Comment').setup()
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
 require('indent_blankline').setup {
-  char = '┊',
   show_trailing_blankline_indent = false,
 }
 
@@ -431,6 +430,20 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+
+
+require('onedark').setup {
+
+  style = 'cool',
+  transparent = false,
+  highlights = {
+    ["@variable.builtin"] = {fg = '#ffffff'},
+    ["@variable"] = {fg = '#ffffff'},
+  }
+}
+
+require('onedark').load()
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
